b2496ee35a97d28d3d0dee6916092cf3d81f31a6
Rathan Kumar <rathan@localhosted.in>
2025-05-07 23:55:50 +0530

    Fixed some syntax issues

--- home/flake.nix
+++ home/flake.nix
@@ -5,21 +5,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    outputs = { self, nixpkgs, home-manager, ... }:
-      let
-        systems = [ "x86_64-linux" "aarch64-darwin" ];
-        mkHomeConfig = system: home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs { inherit system; };
-          modules = [ ./home.nix ];
-          # username and homeDirectory detected at runtime
-        };
-      in {
-        homeConfigurations = builtins.listToAttrs (
-          map (system: {
-            name = "${system}-default";
-            value = mkHomeConfig system;
-          }) systems
-        );
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      mkHomeConfig = system: home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { inherit system; };
+        modules = [ ./home.nix ];
+        # username and homeDirectory detected at runtime
       };
-  }
+    in {
+      homeConfigurations = builtins.listToAttrs (
+        map (system: {
+          name = "${system}-default";
+          value = mkHomeConfig system;
+        }) systems
+      );
+    };
 }
